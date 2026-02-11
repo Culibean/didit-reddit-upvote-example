2f32f2284d5f4d3148c31beb6769808c668ec1fa
ULRIKE KUNZE <ULRIKE.E.KUNZE@GMAIL.COM>
2026-02-11 11:33:29 +0000

    feat: date sorting button

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,8 +3,19 @@ import { Pagination } from "./Pagination";
 import { Vote } from "./Vote";
 import { db } from "@/db";
 import { POSTS_PER_PAGE } from "@/config";
+import { GrRestaurant } from "react-icons/gr";
 
-export async function PostList({ currentPage = 1 }) {
+export async function PostList({ currentPage = 1, searchParams }) {
+  // TODO: add option to sort posts by date and votes
+
+  let dateOrder = "vote_total DESC";
+
+  if (searchParams?.sort === "date_desc") {
+    dateOrder = "diditposts.created_at DESC";
+  } else if (searchParams?.sort === "date_asc") {
+    dateOrder = "diditposts.created_at ASC";
+  }
+
   const { rows: posts } =
     await db.query(`SELECT diditposts.id, diditposts.title, diditposts.body, diditposts.created_at, users.name, 
     COALESCE(SUM(votes.vote), 0) AS vote_total
@@ -12,12 +23,42 @@ export async function PostList({ currentPage = 1 }) {
      JOIN users ON diditposts.user_id = users.id
      LEFT JOIN votes ON votes.post_id = diditposts.id
      GROUP BY diditposts.id, users.name
-     ORDER BY vote_total DESC
+     ORDER BY ${dateOrder}
      LIMIT ${POSTS_PER_PAGE}
      OFFSET ${POSTS_PER_PAGE * (currentPage - 1)}`);
 
+  // const queryString = (await searchParams) || {};
+
+  // posts.forEach((item) => {
+  //   item.created_at = new Date(item.created_at);
+  // });
+
+  // if (queryString.sort === "date_desc") {
+  //   posts.sort((a, b) => b.created_at - a.created_at);
+  // } else if (queryString.sort === "date_asc") {
+  //   posts.sort((a, b) => a.created_at - b.created_at);
+  // }
+  // console.log(posts);
+
   return (
     <>
+      {/* TODO: Add sorting buttons to page */}
+      <section className="flex justify-center gap-4 mt-6">
+        <Link
+          className="bg-pink-300 text-black px-3 py-2 rounded"
+          href="/?sort=date_desc"
+        >
+          {" "}
+          Newest first{""}
+        </Link>
+        <Link
+          className="bg-pink-300 text-black px-3 py-2 rounded"
+          href="/?sort=date_asc"
+        >
+          {" "}
+          Oldest first{""}
+        </Link>
+      </section>
       <ul className="max-w-screen-lg mx-auto p-4 mb-4">
         {posts.map((post) => (
           <li
