ef4431cb881babef7d83cd2ca9eb7891747b0e55
ULRIKE KUNZE <ULRIKE.E.KUNZE@GMAIL.COM>
2026-02-11 15:24:37 +0000

    feat: added design

--- src/app/profile/[userid]/page.jsx
+++ src/app/profile/[userid]/page.jsx
@@ -25,18 +25,26 @@ export default async function ProfilePage({ params }) {
 
   return (
     <>
-      <div>
-        <h1>{profile.name}s Profile</h1>
-        <div>
-          <h2>Preferred Name: {userInfo.preferred_name}</h2>
-          <h2>Country: {userInfo.country}</h2>
-          <h2>Age: {userInfo.age}</h2>
-          <h2>Favourite Game: {userInfo.favourite_game}</h2>
-          <h2>Inspirational Quote: {userInfo.inspo_quote}</h2>
+      <div className="max-w-screen-lg mx-auto p-4 bg-zinc-300 mt-10 rounded-xl">
+        <h1 className="text-3xl mb-4">{profile.name}s Profile</h1>
+        <div className="space-y-2">
+          <h2 className="bg-zinc-100 px-3 py-2 rounded">
+            Preferred Name: {userInfo.preferred_name}
+          </h2>
+          <h2 className="bg-zinc-100 px-3 py-2 rounded">
+            Country: {userInfo.country}
+          </h2>
+          <h2 className="bg-zinc-100 px-3 py-2 rounded">Age: {userInfo.age}</h2>
+          <h2 className="bg-zinc-100 px-3 py-2 rounded">
+            Favourite Game: {userInfo.favourite_game}
+          </h2>
+          <h2 className="bg-zinc-100 px-3 py-2 rounded">
+            Inspirational Quote: {userInfo.inspo_quote}
+          </h2>
         </div>
         <a
           href={`/profileupdate/${userId}`}
-          className="bg-pink-300 text-black px-4 py-2 rounded w-full"
+          className="inline-block bg-green-400 px-4 py-2 text-xl mt-4 text-black rounded"
         >
           Edit Profile
         </a>
--- src/app/profileupdate/[userid]/page.jsx
+++ src/app/profileupdate/[userid]/page.jsx
@@ -5,33 +5,49 @@ import { updateProfile } from "@/actions/updateUser";
 export default function UserProfil() {
   return (
     <>
-      <div>
-        <form action={updateProfile}>
+      <div className="max-w-screen-lg mx-auto p-4 bg-zinc-300 mt-10 rounded-xl">
+        <form action={updateProfile} className="flex flex-col space-y-4">
           {/* Preferred Name */}
           <label>
             Preferred name: {""}
-            <input type="text" name="preferred_name" />
+            <input
+              type="text"
+              name="preferred_name"
+              className="bg-zinc-100 px-3 py-2 rounded"
+            />
           </label>
 
           {/* Country */}
 
           <label>
             Country: {""}
-            <input type="text" name="country" />
+            <input
+              type="text"
+              name="country"
+              className="bg-zinc-100 px-3 py-2 rounded"
+            />
           </label>
 
           {/* Age */}
 
           <label>
             Age: {""}
-            <input type="number" name="age" />
+            <input
+              type="number"
+              name="age"
+              className="bg-zinc-100 px-3 py-2 rounded"
+            />
           </label>
 
           {/* Favourite Game */}
 
           <label>
             Favourite Game: {""}
-            <input type="text" name="favourite_game" />
+            <input
+              type="text"
+              name="favourite_game"
+              className="bg-zinc-100 px-3 py-2 rounded"
+            />
           </label>
 
           {/* Inspo Quote */}
@@ -39,6 +55,7 @@ export default function UserProfil() {
           <label>
             Inspirational Quote: {""}
             <textarea
+              className="bg-zinc-100 px-3 py-2 rounded"
               name="inspo_quote"
               placeholder="The one quote that guides you..."
             />
@@ -46,7 +63,7 @@ export default function UserProfil() {
 
           <button
             type="submit"
-            className="bg-pink-300 text-black px-4 py-2 rounded w-full"
+            className="inline-block bg-green-400 px-4 py-2 text-xl mt-4 text-black rounded"
           >
             Save Profile
           </button>
